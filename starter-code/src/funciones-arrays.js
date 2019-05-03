c516dae719bc6a4027e41f835291591d6438b034
joll <joll@joll.com>
2019-05-03 14:33:05 +0200

    primero

--- starter-code/src/funciones-arrays.js
+++ starter-code/src/funciones-arrays.js
@@ -1,4 +1,12 @@
 // Find the maximum
+function maxOfTwoNumbers(n1,n2){
+    
+  if(n1>n2){
+    return n1;
+  }else{
+    return n2;
+  }
+};
 
 // Finding Longest Word
 let words = [
@@ -11,6 +19,9 @@ let words = [
   'crackpot'
 ];
 
+function findLongestWord(words){
+  words.length();
+}
 // Calculating a Sum
 
 let numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
